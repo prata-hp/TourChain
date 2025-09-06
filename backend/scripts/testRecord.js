766d0e20d81197b2ac7c22ab28b47fe39ca92ac2
Anupam Karmakar <anupamkarmakar27@gmail.com>
2025-09-06 15:27:28 +0530

    testRecord Updated

--- backend/scripts/testRecord.js
+++ backend/scripts/testRecord.js
@@ -15,18 +15,3 @@ const { recordJourney, toIdHashHex } = require("../utils/blockchain");
     console.error("Error running testRecord:", err);
   }
 })();
-
-const hre = require("hardhat");
-
-async function main() {
-  const Factory = await hre.ethers.getContractFactory("TouristRegistry");
-  const contract = await Factory.deploy();
-  await contract.waitForDeployment();
-  const addr = await contract.getAddress();
-  console.log("TouristRegistry deployed to:", addr);
-}
-
-main().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
