fb2325aa3147e7aad7ec0f07be96395134dad31f
Sambhas Maiti <sombasmaity@gmail.com>
2025-09-29 13:39:56 +0530

    Update ActiveJourney.js

--- police_backend/models/ActiveJourney.js
+++ police_backend/models/ActiveJourney.js
@@ -5,9 +5,7 @@ const activeJourneySchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     itinerary: { type: Object, required: true },
     
-    // --- THIS IS THE FIX ---
-    // Change the type from an array of ObjectIDs to an array of generic Objects.
-    // This allows you to store the "snapshots" your controller is creating.
+    
     members: [Object], 
 
     startDate: { type: Date, required: true },
@@ -29,5 +27,6 @@ const activeJourneySchema = new mongoose.Schema({
         panic: { type: String }
     }
 }, { timestamps: true });
+
 
 module.exports = mongoose.model('ActiveJourney', activeJourneySchema);
