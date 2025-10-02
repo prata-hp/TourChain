3c6bd241faae301a9f5231628ca278ce922ba485
archxnna <tiwarirchana144@gmail.com>
2025-10-02 21:13:34 +0530

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        // --- THIS IS THE NEW, DETAILED LOG ---
         console.log(`✅ MongoDB Connected: HOST=${conn.connection.host} DB=${conn.connection.name}`);
     } catch (error) {
         console.error(`❌ MongoDB Connection Error: ${error.message}`);
